95252320acb381373bf15129401f9290ad621345
jonahmakori <jonahmakori0@gmail.com>
2020-02-11 18:10:23 +0300

    Created a sighting class and its test class Also modified other classes then created templates for animal,endangered animals,index etc and the layout

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,68 +1,99 @@
 package models;
-
-import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class AnimalTest {
+
+public class AnimalTest{
+
     @Rule
     public DatabaseRule database = new DatabaseRule();
 
-    @After
-    public void tearDown() {Animal.clearAllAnimals();
-    }
-    @Test
-    public void TestThatInstanceOfAnimalIsCreated(){
-        Animal testAnimal = new Animal("Lion");
-        assertEquals(true,testAnimal instanceof Animal);
-    }
+    private Animal testAnimal;
+    private EndangeredAnimals testEndangered;
 
-    @Test
-    public void TestThatAnimalInstantiatesWithName_Lion(){
-        Animal testAnimal = new Animal("Lion");
-        assertEquals("Lion",testAnimal.getName());
-    }
-    @Test
-    public void EqualTestThatReturnsTrueIfNameAndAnotherAnimalsNameAreSame(){
-        Animal firstAnimal = new Animal("Lion");
-        Animal anotherAnimal = new Animal("Lion");
-        assertTrue(firstAnimal.equals(anotherAnimal));
+    @Before
+    public void setup(){
+        testAnimal = new Animal("Monkey");
+        testEndangered =new EndangeredAnimals("Zebra","Healthy","Young");
 
     }
 
     @Test
-    public void TestSaveInsertObjectAnimalIntoDatabase(){
-        Animal testAnimal = new Animal("Lion");
+    public void animal_instantiatesCorrectly_true() {
+        assertEquals(true, testAnimal instanceof Animal);
+    }
+
+    @Test
+    public void getName_animalInstantiatesWithName_Orangutan() {
+        assertEquals("Monkey", testAnimal.getName());
+    }
+
+    @Test
+    public void getId_animalInstantiatesWithId(){
         testAnimal.save();
-        assertTrue(Animal.all().get(0).equals(testAnimal));
+        assertTrue(testAnimal.getId() > 0);
     }
+
     @Test
-    public void TestThatReturnsAllInstancesOfAnimal(){
-        Animal firstAnimal = new Animal("Lion");
-        firstAnimal.save();
-        Animal secondAnimal = new Animal("Buffalo");
-        secondAnimal.save();
-        assertEquals(true, Animal.all().get(0).equals(firstAnimal));
-        assertEquals(true, Animal.all().get(1).equals(secondAnimal));
+    public void getAge_animalInstantiatesWithAge_Young() {
+
+        assertEquals("Young", testEndangered.getAge());
     }
+
+    @Test
+    public void getHealth_animalInstantiatesWithHealth_Healthy() {
+        assertEquals("Healthy", testEndangered.getHealth());
+    }
+
+    @Test
+    public void getType_animalInstantiatesWithType_Type() {
+        assertEquals("Non-endangered", testAnimal.getType());
+    }
+
+    @Test
+    public void equals_returnsTrueIfNamesAreTheSame() {
+        Animal anotherAnimal = new Animal("Monkey");
+        assertTrue(testAnimal.equals(anotherAnimal));
+    }
+
     @Test
     public void save_assignsIdToObject() {
-        Animal testAnimal = new Animal("Lion");
         testAnimal.save();
         Animal savedAnimal = Animal.all().get(0);
         assertEquals(testAnimal.getId(), savedAnimal.getId());
     }
 
     @Test
-    public void find_returnsAnimalWithSameId_secondAnimal() {
-        Animal firstAnimal = new Animal("Lion");
-        firstAnimal.save();
-        Animal secondAnimal = new Animal("Buffalo");
-        secondAnimal.save();
-        assertEquals(Animal.find(secondAnimal.getId()), secondAnimal);
+    public void save_insertsObjectIntoDatabase() {
+        testAnimal.save();
+        assertTrue(Animal.all().get(0).equals(testAnimal));
     }
 
+    @Test
+    public void all_returnsAllInstancesOfAnimal_false() {
+        testAnimal.save();
+        Animal otherAnimal = new Animal("Deer");;
+        otherAnimal.save();
+        assertEquals(true, Animal.all().get(0).equals(testAnimal));
+        assertEquals(true, Animal.all().get(1).equals(otherAnimal));
+    }
+
+    @Test
+    public void find_returnsAnimalWithSameId() {
+        testAnimal.save();
+        Animal anotherAnimal = new Animal("Deer");
+        anotherAnimal.save();
+        assertEquals(Animal.find(anotherAnimal.getId()), anotherAnimal);
+    }
+
+    @Test
+    public void delete_deletesAnimal() {
+        testAnimal.save();
+        testAnimal.delete();
+        assertEquals(0, Animal.all().size());
+    }
 }
--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -1,22 +1,22 @@
 package models;
-
 import org.junit.rules.ExternalResource;
 import org.sql2o.*;
-
-public class DatabaseRule extends ExternalResource{
+public class DatabaseRule extends ExternalResource {
 
     @Override
-    protected void before(){
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test","jonahmakori","jonah1ma");
-
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "jonahmakori", "jonah1ma");
     }
+
     @Override
-    protected void after(){
-        try(Connection con = DB.sql2o.open()){
-            String deleteAnimalsQuery = "DELETE FROM persons *;";
+    protected void after() {
+        String deleteAnimalsQuery = "DELETE FROM animals *;";
+        String deleteEndangeredAnimalsQuery = "DELETE FROM endangered_animals *;";
+        String deleteSightingsQuery = "DELETE FROM sightings *;";
+        try(Connection con = DB.sql2o.open()) {
             con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(deleteEndangeredAnimalsQuery).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
         }
     }
-
-
 }
