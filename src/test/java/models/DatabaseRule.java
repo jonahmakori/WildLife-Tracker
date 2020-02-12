d46bbe3fd82ad527d284e33ce4eade5a62c7e144
jonahmakori <jonahmakori0@gmail.com>
2020-02-12 08:29:25 +0300

    Added content to the templates and modified the templates

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -5,13 +5,13 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "jonahmakori", "jonah1ma");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "jonah", "jonah1ma");
     }
 
     @Override
     protected void after() {
         String deleteAnimalsQuery = "DELETE FROM animals *;";
-        String deleteEndangeredAnimalsQuery = "DELETE FROM endangered_animals *;";
+        String deleteEndangeredAnimalsQuery = "DELETE FROM endangeredAnimals *;";
         String deleteSightingsQuery = "DELETE FROM sightings *;";
         try(Connection con = DB.sql2o.open()) {
             con.createQuery(deleteAnimalsQuery).executeUpdate();
